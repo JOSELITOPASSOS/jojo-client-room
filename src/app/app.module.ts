0d5d7ea2d726a1c775d4c2572c2ed19c44bdeac9
unknown <joselitomartinho@gmail.com>
2021-03-29 20:08:11 -0300

    Add componentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { RooDetailsComponent } from './roo-details/roo-details.component';
+import { RoomListComponent } from './room-list/room-list.component';
+import { RoomDetailsComponent } from './room-details/room-details.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent,
+    RooDetailsComponent,
+    RoomListComponent,
+    RoomDetailsComponent,
+    UpdateRoomComponent
   ],
   imports: [
     BrowserModule
